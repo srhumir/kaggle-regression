8bd362b9b64274c71ffa576d5865313233269b93
Reza <srh.umir@gmail.com>
2016-10-13 15:52:06 +0200

    more feature selection

--- code.py
+++ code.py
@@ -5,26 +5,28 @@ Created on Wed Oct 12 12:24:54 2016
 @author: Hanireza
 """
 
-import zipfile as zip
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import theano
+#import theano
 import sklearn.preprocessing as pp
 import sklearn.cross_validation as cv
 import math
 import sklearn.ensemble as en
-import sklearn.pipeline as pip
+#import sklearn.pipeline as pip
 import sklearn.metrics as metrics
 import time
 #__location__ = os.path.realpath(
 #    os.path.join(os.getcwd(), os.
 #sys.argv[0]
 #))
-file = zip.ZipFile('train.csv.zip', "c:/Reza/python/regression/Kaggle")
-file.namelist()
-file.extractall()
-data_known = pd.read_csv("c:/Reza/python/regression/Kaggle/train.csv")
+#file = zip.ZipFile('train.csv.zip', "c:/Reza/python/regression/Kaggle")
+#file.namelist()
+#file.extractall()
+#import urllib2
+#response = urllib2.urlopen('https://github.com/srhumir/kaggle-regression/raw/master/train.csv')
+#html = response.read()
+data_known = pd.read_csv("https://github.com/srhumir/kaggle-regression/raw/master/train.csv")
 loss = data_known["loss"]
 loss = np.array(loss)
 #loss = loss.astype(theano.config.floatX)
@@ -54,36 +56,67 @@ for f in range(50):
 
 #plt.scatter(X_train["cont7"], loss_train)
 #plt.hist(loss, bins=1000)
-thress = np.array(range(79,100))/100
-thress = np.append(thress, .999)
-number_vars=[]
-train_err = []
-#threshold = .79
-#val_err=[]
-t0_featselec = time.time()
-for threshold in thress:
-    choose = np.where(cumsum >= threshold)[0][0]
-    impvars = indices[:choose]
-    err = cv.cross_val_score(estimator=rf,
-                          X=np.array(X_train)[:,impvars],
+
+
+#==============================================================================
+# a = (1-.999)/20
+# thress = [.99+a*i for i in range(20)]
+# #for i in range(20):
+# #    thress.append(thress[-1]+a)
+# 
+# thress = np.array(range(999,99999))/100000
+# thress = np.append(thress, .999)
+# number_vars=[]
+# train_err = []
+# cv_err =[]
+# #threshold = .79
+# #val_err=[]
+# t0_featselec = time.time()
+# for threshold in thress:
+#     choose = np.where(cumsum >= threshold)[0][0]
+#     impvars = indices[:choose]
+#     err = cv.cross_val_score(estimator=rf,
+#                           X=X_train.iloc[:,impvars],
+#                           y=loss_train,
+#                           scoring = 'mean_absolute_error',
+#                           cv=5,
+#                           n_jobs=-1)
+# #    rf.fit(np.array(X_train)[:, impvars], loss_train)
+#     number_vars.append(choose+1)
+#     cv_err.append(err)
+#     print(threshold)
+#     print('So far %d seconds' %(time.time() - t0_featselec))
+# print('Total time: %d seconds' %(time.time()-t0_featselec))
+#==============================================================================
+#    val_err.append(math.sqrt(metrics.mean_squared_error(loss_val,rf.predict(X_val_std[:, impvars]))))
+chooses = [int(X_train.shape[1]/40)*i for i in range(42)]
+def check_imp(chooses, rfmodel, X_train, y_train, indices):
+    train_err = []
+    cv_err =[]
+    t0_featselec = time.time()
+    for choose in chooses:
+        impvars = indices[:choose+1]
+        err = cv.cross_val_score(estimator=rf,
+                          X=X_train.iloc[:,impvars],
                           y=loss_train,
                           scoring = 'mean_absolute_error',
                           cv=5,
                           n_jobs=-1)
-#    rf.fit(np.array(X_train)[:, impvars], loss_train)
-    number_vars.append(choose+1)
-    cvn_err.append(err)
-    print(threshold)
+    cv_err.append(err)
+    print(choose)
     print('So far %d seconds' %(time.time() - t0_featselec))
-print('Total time: %d' %(time.time()-t0_featselec))
-    
-#    val_err.append(math.sqrt(metrics.mean_squared_error(loss_val,rf.predict(X_val_std[:, impvars]))))
+print('Total time: %d seconds' %(time.time()-t0_featselec))
 
+cv_err = np.array(cv_err)
 fig=plt.figure(figsize=(10,10))
 plt.subplot()
-plt.plot(thress, cv_err,
+plt.plot(chooses, -cv_err.mean(axis=1),
          color='blue', marker='o',
-         label='Training error')
+         label='Cross validation error')
+plt.fill_between(chooses,
+                 -cv_err.mean(axis=1) - cv_err.std(axis=1),
+                 -cv_err.mean(axis=1) + cv_err.std(axis=1),
+                 alpha=0.15, color='green')
 #plt.plot(number_vars, val_err,
 #         color='green', marker='s',
 #         label='Validation error')
