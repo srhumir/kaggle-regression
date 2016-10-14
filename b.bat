001601d0747808074e8ed628fc871dbf4f44a6ae
Reza <srh.umir@gmail.com>
2016-10-14 12:32:08 +0200

    batch file

---
+++ b.bat
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+cd ~
+mkdir Documents
+cd Documents
+wget "https://github.com/srhumir/kaggle-regression/raw/master/loss_train.csv"
+wget "https://github.com/srhumir/kaggle-regression/raw/master/X_train.csv"
+wget "https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh"
+bash Anaconda3-4.2.0-Linux-x86_64.sh
+pip install Theano
+pip install keras
+cd ~
+mkdir .keras
+cd .keras
+wget "https://github.com/srhumir/kaggle-regression/raw/master/keras.json"
+cd ~
+cd Documents
+jupyter console
