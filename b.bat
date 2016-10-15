f76f5cb413cc334f0d22b3606ee6393fd47ddcc6
Reza <srh.umir@gmail.com>
2016-10-15 10:02:40 +0200

    b.bat update

--- b.bat
+++ b.bat
@@ -7,6 +7,8 @@ wget "https://github.com/srhumir/kaggle-regression/raw/master/loss_train.csv"
 wget "https://github.com/srhumir/kaggle-regression/raw/master/X_train.csv"
 wget "https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh"
 bash Anaconda3-4.2.0-Linux-x86_64.sh
+conda uninstall scikit-learn
+pip install sklearn
 pip install Theano
 pip install keras
 cd ~
