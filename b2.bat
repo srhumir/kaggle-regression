d0edc0eebe406444f0c22aa3f4605818618585f3
Reza <srh.umir@gmail.com>
2016-10-26 10:10:14 +0200

    b2

---
+++ b2.bat
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+conda uninstall scikit-learn
+pip install sklearn
+pip install Theano
+pip install keras
+cd ~
+mkdir .keras
+cd .keras
+wget "https://github.com/srhumir/kaggle-regression/raw/master/keras.json"
+cd ~
+cd Documents
+wget "https://github.com/srhumir/kaggle-regression/raw/master/boost_for_aws.py" 
+python boost_for_aws.py
